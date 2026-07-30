0a3316c4b6930d131bf7d81b9a72088e39849d5e
Gonzalo_Lazarte <lazarte.gonzalo7000@gmail.com>
2026-07-29 22:25:24 -0300

    added_user_menu

---
+++ Camera-View/Camera-View/Actions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camera_View {
+    internal class Actions {
+
+        public Actions() {
+        }
+        public void seleccMenu(string actionSelected) {
+            switch (actionSelected) {
+                case "Ver Camaras":
+                    Console.WriteLine("Ver Camaras");
+                    break;
+                case "Añadir Camaras":
+                    Console.WriteLine("Añadir Camaras");
+                    break;
+                case "Quitar Camaras":
+                    Console.WriteLine("Quitar Camaras");
+                    break;
+
+            }
+        }
+
+    }
+}
--- Camera-View/Camera-View/Program.cs
+++ Camera-View/Camera-View/Program.cs
@@ -6,7 +6,52 @@ using System.Threading.Tasks;
 
 namespace Camera_View {
     internal class Program {
+        static Actions menuAction = new Actions();
         static void Main(string[] args) {
+            
+            string[] option = {"Ver Camaras", "Añadir Camaras", "Quitar Camaras", "Salir"};
+            int selection = 0;
+            while (true) {
+                Console.Clear();
+                Console.WriteLine("=== MENU ===\n");
+                for (int i = 0; i < option.Length; i++) {
+                    if (i == selection) {
+                        Console.WriteLine($"<< {option[i]} >>");
+                    }
+                    else {
+                        Console.WriteLine($"   {option[i]}");
+                    }
+                }
+                Console.WriteLine("");
+                Console.WriteLine("===========\n");
+                ConsoleKey tecla = Console.ReadKey(true).Key;
+                switch (tecla) {
+                    case ConsoleKey.UpArrow:
+                        if (selection > 0) {
+                            selection--;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (selection < option.Length - 1) { 
+                            selection++;
+                        }
+                        break;
+                    case ConsoleKey.Enter:
+                        Console.Clear();
+                        Console.WriteLine($"Elegiste: {option[selection]}");
+
+                        if (option[selection] == "Salir") {
+                            return;
+                        }
+                        else {
+                            menuAction.seleccMenu(option[selection]);
+                        }
+                        return;
+                        
+                }
+            }
+
+            //aqui llamamos al metodo para la salida del programa
         }
     }
 }
